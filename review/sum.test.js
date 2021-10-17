4f8fd5c428f255d9eb555c1c399924b28ffa7e58
sagarchhabra1 <91247498+sagarchhabra1@users.noreply.github.com>
2021-10-18 01:01:01 +0530

    Delete sum.test.js

--- review/sum.test.js
+++
@@ -1,9 +0,0 @@
-const add=require('./sum');
-
-test('Sum of two given no. is',()=>{
-  expect(add(5,5)).toBe(10);
-})
-
-test('Sum of two given no. is not',()=>{
-  expect(add(5,5)).not.toBe(9);
-})
