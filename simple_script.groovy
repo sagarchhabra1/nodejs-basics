7ad7ee6e484d99a0cb4b7e785587a33002fad2ff
sagarchhabra1 <91247498+sagarchhabra1@users.noreply.github.com>
2023-09-27 18:24:31 +0530

    Create simple_script.groovy

---
+++ simple_script.groovy
@@ -0,0 +1,23 @@
+// This is a comment
+
+// Echoing a message to the console
+println "Hello, World!"
+
+// You can also use single or double quotes
+echo 'This is a single-quoted message'
+
+// Defining variables
+def firstName = "John"
+def lastName = "Doe"
+
+// Interpolating variables in a string
+println "My name is ${firstName} ${lastName}"
+
+// Using arithmetic operations
+def x = 5
+def y = 3
+def sum = x + y
+def product = x * y
+
+println "Sum of x and y: ${sum}"
+println "Product of x and y: ${product}"
