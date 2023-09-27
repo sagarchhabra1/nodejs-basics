da64c277b6fab9e23782d4211abef43cfdcfdd86
sagarchhabra1 <91247498+sagarchhabra1@users.noreply.github.com>
2023-09-27 12:53:43 +0530

    Update testing-action.groovy

--- testing-action.groovy
+++ testing-action.groovy
@@ -8,7 +8,7 @@ pipeline {
     stages{
         stage('Checkout'){
             steps{
-            checkout([$class: 'GitSCM', branches: [[name: '*/main']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '115f51a9517edc2a1bf5c6602aa7b0b451', url: 'https://github.com/sagarchhabra1/nodejs-basics.git']]])
+            checkout([$class: 'GitSCM', branches: [[name: '*/main']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '62b47fbc-4b55-4b96-bc70-ab5f202c69ae', url: 'https://github.com/sagarchhabra1/nodejs-basics.git']]])
                       
             }
         }
