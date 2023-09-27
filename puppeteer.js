7ef399f616b0e4fdfa033cac92897f6e81fa330e
sagarchhabra1 <91247498+sagarchhabra1@users.noreply.github.com>
2023-09-27 17:04:44 +0530

    Update puppeteer.js

--- puppeteer.js
+++ puppeteer.js
@@ -12,4 +12,4 @@ async function run() {
   await new Promise(resolve => setTimeout(resolve, 5000));
   await browser.close();
 }
-#this is the sample code cvofinenghn ..
+#this is the szxxample code cvofinenghn ..
