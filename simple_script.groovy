918a3b5a1a28daa1a52badc87fafe25ed5a3fab2
sagarchhabra1 <91247498+sagarchhabra1@users.noreply.github.com>
2023-10-05 13:05:11 +0530

    Update simple_script.groovy

--- simple_script.groovy
+++ simple_script.groovy
@@ -7,5 +7,19 @@ pipeline {
                 cleanWs()
             }
         }
+
+        stage('Setup parameters'){
+            steps{
+                script{
+                    properties(
+                        [[$class: 'JiraProjectProperty'], 
+                        [$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false], 
+                        parameters([
+                            string(defaultValue: '3414', description: '', name: 'RUN_ID', trim: false)
+                        )]
+                    )
+                }
+            }
+        }
     }
 }
