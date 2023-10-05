aef72c995f9ca93a7d5f2a998438ceedf840b7de
sagarchhabra1 <91247498+sagarchhabra1@users.noreply.github.com>
2023-10-05 13:14:38 +0530

    Update simple_script.groovy

--- simple_script.groovy
+++ simple_script.groovy
@@ -14,8 +14,7 @@ pipeline {
                     properties(
                         parameters([
                             string(defaultValue: '3414', description: '', name: 'RUN_ID', trim: false)
-                        ]),
-                        [$class: 'JobLocalConfiguration', changeReasonComment: '']]
+                        ])
                     )
                 }
             }
